78b17b83572ee49faf7dd90ef85e8e8022b728f8
Lars <larsmackenzie@gmail.com>
2020-02-18 19:52:05 -0500

    most fitler functionality is working

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,10 +7,10 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      icons: ['email.png', 'facebook.png', 'twitter.png', 'linkedin.png'],
       keyword: '',
-      group_id: 0, 
-      keywords: []
+      group_id: 0,
+      seeMore: [],
+      see: false
     }
   }
   addKeywords = () => {
@@ -28,34 +28,46 @@ class Home extends Component {
       ...this.state, 
       group_id: group_id
     })
-
+    // when page loads, sort by group id. 
+    this.props.dispatch({type: 'SORT_DATA', payload: {group: group_id, keywords: this.props.keywords}});
   }
-  
-
+  getMore = () => {
+    let more = this.props.data;
+   //figure out how to remove just the first item without altering props. 
+    console.log(more);
+    this.setState({
+      ...this.state,
+      seeMore: more,
+      see: true
+    })
+  }
   handleChange = (event) => {
     this.setState({
       ...this.state,
       keyword: event.target.value
     })
   }
-
   render() {
+    let topStory = [];
+    if(this.props.data.length > 0) {
+      topStory.push(this.props.data[0]);
+    }
+    
     return (
       <div>
         <div className="nav-bar">
-          {JSON.stringify(this.props.keywords)}
           <p className="helper-text">Not seeing what you want? Improve our algorithm by telling us your favorite topics. One at a time, please.</p>
           <div className="flex-box flex-center"> <input value={this.state.keyword} onChange={(event) => this.handleChange(event)} className="column-3" /><button onClick={this.addKeywords}>Submit</button></div>
         </div>
         <div className="header">
           <h1>Your Top Story</h1>
-          {this.props.data.map((story, i) => {
+          {topStory.map((story, i) => {
             return (
               <div key={i}>
                 <h2>{story.title}</h2>
                 <div><PostBody post={story} /></div>
                 <div className="flex-box">
-                  {this.state.icons.map((icon, i) => {
+                  {this.props.icons.map((icon, i) => {
                     return (<img key={i} className="icon" src={require(`../images/${icon}`)} alt="social media" />)
                   })}
                 </div>
@@ -63,14 +75,28 @@ class Home extends Component {
             )
           })}
         </div>
-        <div className="nav-bar"><span className="link">See more</span></div>
-
+        {!this.state.see && <div className="nav-bar"><span className="link" onClick={()=>this.getMore()}>See more</span></div>}
+         {this.state.see && <div className="main-body">{this.state.seeMore.map((story, i) => {
+             return (
+              <div key={i}>
+                <h2>{story.title}</h2>
+                <div><PostBody post={story} /></div>
+                <div className="flex-box">
+                  {this.props.icons.map((icon, i) => {
+                    return (<img key={i} className="icon" src={require(`../images/${icon}`)} alt="social media" />)
+                  })}
+                </div>
+              </div>
+            )
+          })}
+        </div>}
 
       </div>)
   }
 }
 const mapStateToProps = state => ({
   keywords: state.keywords.keywords,
-  data: state.data.articles
+  data: state.data.articles,
+  icons: state.data.icons
 })
 export default connect(mapStateToProps)(Home); 
--- src/redux/reducers/data.js
+++ src/redux/reducers/data.js
@@ -1,12 +1,55 @@
 
 import { combineReducers } from 'redux';
 
-const articles = (state = [{ title: 'title', body: '<p>Body</p>', image: 'something.png', showRating: 1 }], action) => {
-    if (action.type === 'SET_ARTICLES'){
+const articles = (state = [{ title: 'title', body: '<p> Body test test2 test1 </p>', image: 'something.png', showRating: 0 }, { title: 'title', body: '<p> Nothing here but test test test test test </p>', image: 'something.png', showRating: 0 }], action) => {
+    if (action.type === 'SET_ARTICLES') {
         return action.payload;
-    } 
-    return state
+    } else if (action.type === 'SORT_DATA') {
+        let postsToAnalyze = state;
+        let keywords = action.payload.keywords;
+        // we need to look through each of our keywords to see if we can find them in our posts.
+        for (let j = 0; j < keywords.length; j++) {
+            // we search through our new array, called postsToAnalyze
+            for (let i = 0; i < postsToAnalyze.length; i++) {
+                // let's name the values of each object, within the body key, in a variable called data
+                let data = postsToAnalyze[i].body;
+                // we're going to use this variable count later to add any occurences of the keyword to the post object
+                let count = postsToAnalyze[i].showRating;
+                // we need to split this string into an array that we can search through, by splitting at all spaces. 
+                let words = data.split(' ');
+                // if we can find any of the keyword, we'll return an index of 0 or greater, which means we should then count all of the times that word appears.
+                let index = words.indexOf(keywords[j].name);
+                if (index >= 0) {
+                    let match = words[index];
+                    let all = words.filter(word => word === match);
+                    let occurrence = all.length;
+                    count += occurrence;
+                }
+                // if we've counted occurences, we'll add it to the original count. if we didn't find anything, we'll simply keep the count as it was, reassigning the showRating key to its original value
+                postsToAnalyze[i].showRating = count;
+            }
+        }
+        // next we need to sort these posts from highest ranked to lowest using this sorting method. 
+        let sorted = postsToAnalyze.sort((a, b) => (b.showRating > a.showRating) ? 1 : -1);
+        return sorted;
+    } else {
+        return state
+    }
+}
+const icons = (state = ['email.png', 'facebook.png', 'twitter.png', 'linkedin.png']) => {
+    return state;
+}
+const loading = (state = true, action) => {
+    if (action.type === 'DONE_LOADING') {
+        return false
+    } else if (action.type === 'STILL_LOADING') {
+        return true;
+    } else {
+        return state;
+    }
 }
 export default combineReducers({
-    articles
+    articles,
+    icons,
+    loading
 });
